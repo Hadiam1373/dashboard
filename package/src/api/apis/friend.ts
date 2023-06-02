b9030f55ca298e8bab79dad91eaf6e2fecbcfcf7
Hadi.Moradi137 <hadimoradi1373@outlook.com>
2023-06-03 00:35:24 +0330

    add api

---
+++ package/src/api/apis/friend.ts
@@ -0,0 +1,14 @@
+import fetch from '@/api/fetch';
+
+class PagingParams {
+}
+
+/**
+ * 群分页消息
+ * @param params
+ */
+export async function getFriendMessage(params: PagingParams) {
+  return await fetch.get(`/friend/friendMessages`, {
+    params,
+  });
+}
---
+++ package/src/api/fetch/interceptors.ts
@@ -0,0 +1,24 @@
+import type { AxiosRequestConfig, AxiosResponse } from 'axios';
+// @ts-ignore
+import cookie from 'js-cookie';
+
+// 请求拦截器
+export const requestSuccess = (request: AxiosRequestConfig) => {
+  const token = cookie.get('token');
+  // @ts-ignore
+  request.headers.token = token;
+  return request;
+};
+
+export const requestFail = (error: AxiosRequestConfig) => {
+  return Promise.reject(error);
+};
+
+// 接收拦截器
+export const responseSuccess = (response: AxiosResponse) => {
+  return response;
+};
+
+export const responseFail = (error: AxiosResponse) => {
+  return Promise.reject(error);
+};
